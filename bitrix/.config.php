8f46d24349b0f3a3be1cbe8ca34e9d7518b413eb
Viktor <witja245@yandex.ru>
2025-07-31 01:06:45 +0300

    первый коммит

---
+++ bitrix/.config.php
@@ -0,0 +1,7 @@
+<?php
+$bxProductConfig = [
+	"intranet_wizard" => [
+		"product_name" => "Настройка «1С-Битрикс24: Корпоративный портал»",
+		"product_image" => '<img src="/bitrix/images/install/ru/box_bitrix24.png" alt="" />',
+	]
+];
